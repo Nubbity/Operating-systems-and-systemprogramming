049039cac8f6b8e955a5f8bab9ae34555c11633c
Santeri <santeri.hynninen@student.lut.fi>
2022-04-11 00:42:20 +0300

    still bug and wount work

--- Project 2: Unix Utilities/my-unzip.c
+++ Project 2: Unix Utilities/my-unzip.c
@@ -6,9 +6,43 @@
     22.2.2022
 */
 
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+void unzipFile(FILE * fpIn, FILE * fpOut){
+    int lenght = 0;
+    char last;
+    if(!fpIn || !fpOut){printf("my-grep: cannot open file\n"); exit(1);}
+    printf("Lezt go\n");
+    while (1)
+    {
+        if(1 != fread(&lenght, sizeof(int), 1, fpIn)){break;}//Read number
+        if(1 != fread(&last, sizeof(char), 1, fpIn)){break;}//Read char
 
-int main(){}
+        for (;lenght > 0; lenght--){
+            fprintf(stdin, "%c", last);
+            }
+        lenght = 0;
+       
+    }
+    fclose(fpOut);
+    fclose(fpIn);
+    
+}
+int main(int argc, char *argv[]){
+    if(argc == 1){fprintf(stderr, "my-unzip: file1(Input) [file2(Output)]\n");exit(1);}
+    else if (argc == 2)
+    {
+        unzipFile(fopen(argv[1], "r"), stdin);
+    }
+    else if (argc == 3){
+        unzipFile(fopen(argv[1], "r"), fopen(argv[2], "w"));
+    }
+    else{
+        fprintf(stderr, "my-zip: file1(Input) [file2(Output)]\n");
+    }
+    return 0;
+}
+
---
+++ Project 2: Unix Utilities/my-zip.c
@@ -0,0 +1,53 @@
+/*  
+    Käyttöjärjestelmät ja systeemiohjelmointi 2022
+    Santeri Hynninen
+    0565519
+    Project 2: Unix Utilities:ZIP
+    22.2.2022
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void zipFile(FILE * fpIn, FILE * fpOut){
+    int current;
+    int last;
+    int n = 1;
+
+    while ((current = fgetc(fpIn))!= EOF)
+    {
+        if(current == last){
+            n++;
+        }
+        else{
+           fwrite(&n, sizeof(int),1, fpOut);
+           fprintf(stdout,"%d", n);
+           fwrite(&last,sizeof(char),1, fpOut);
+           fprintf(stdout,"%c", last);
+           n = 1;
+        }
+        last = current;
+    }
+    
+}
+int main(int argc, char *argv[]){
+    //No files
+    
+    if(argc == 1){fprintf(stderr, "my-zip: file1 [file2 ...]\n");exit(1);}
+    else if (argc == 2)
+    {
+        zipFile(stdin, fopen(argv[1], "w"));
+    }
+    else{
+    //käydään kaikki tiedostot läpi
+    for (int i = 2; i < argc; i++)
+    {   
+        FILE* fp = fopen(argv[i], "r");//Tiedoston avaaminen
+        if(!fp){printf("my-grep: cannot open file\n"); exit(1);}//Jos tiedostoa ei löydy/ei voi avata
+        zipFile(fp,fopen(argv[1], "a"));
+        fclose(fp);
+    }}
+    return 0;
+}
+
