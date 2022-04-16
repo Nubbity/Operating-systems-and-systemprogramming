3d7b4549b3bf8f7edb6f55a047cdf2514c4c324d
Santeri <santeri.hynninen@student.lut.fi>
2022-04-17 01:07:46 +0300

    Most of build ins done, pararell executing works... Need to make the path work

--- Project 3: Unix Shell/wish.c
+++ Project 3: Unix Shell/wish.c
@@ -10,7 +10,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include<sys/wait.h>
 #include <unistd.h>
+#include <limits.h>
 
 char** tokenize(char* str, char *spacer)
 {
@@ -24,7 +26,48 @@ char** tokenize(char* str, char *spacer)
     array[i++] = NULL;//Null argument as the last argument for execvp
     return array;
 }
+//Default build in commands
+int defCmd(char ** arguments){//return 1 if not found and 0 if one found
+    char error_message_arguments_exit[50] = "not able to use given arguments with exit\n";
+    char error_message_arguments_path[50] = "Give excactly one argument with cd\n";
+    char error_message_owd[50] = "failed to get owd\n";
+    char error_message_cwd[50] = "failed to get cwd\n";
+    if(!strcmp(arguments[0], "exit")){
+        if(!arguments[1]){
+            printf("Exiting wish\n");
+            exit(0);}
+        else{
+              write(STDERR_FILENO, error_message_arguments_exit, strlen(error_message_arguments_exit));
+        }
+        return 1;
+    }
+    else  if(!strcmp(arguments[0], "path")){
 
+        return 1;}
+    else  if(!strcmp(arguments[0], "cd")){
+        if(!arguments[1] || arguments[2]){
+            write(STDERR_FILENO, error_message_arguments_path, strlen(error_message_arguments_path));
+        }
+        else{
+            char owd[PATH_MAX];
+            char cwd[PATH_MAX];
+            if (getcwd(owd, sizeof(owd))){
+                printf("Old working dir: %s\n", owd);}
+            else{write(STDERR_FILENO, error_message_owd, strlen(error_message_owd));}
+            chdir(arguments[1]);
+            if (getcwd(cwd, sizeof(cwd))) {
+                printf("New working dir: %s\n", cwd);}
+            else{write(STDERR_FILENO, error_message_owd, strlen(error_message_owd));}
+        }
+        return 1;
+    }
+    else  if(!strcmp(arguments[0], "help")){
+        printf("commands: exit, path, cd\n");
+        return 1;
+    }
+    else{
+        return 0;}
+}
 
 int main(int argc, char *argv[]){
     FILE * fpIn;
@@ -36,10 +79,8 @@ int main(int argc, char *argv[]){
         perror("Unable to allocate buffer");
         exit(1);
     }
-
     if(argc == 1){
         fpIn = stdin;
-        printf("stdin\n");
 
     
     }
@@ -56,19 +97,25 @@ int main(int argc, char *argv[]){
     }
     else{exit(1);}
 
-
+    int status;
+    char spacer[2] = " ";
+    if(argc == 1){printf("wish>");}
     //wish loop starts
         while(1){
-            if(argc == 1){printf("wish>");}
-            if(getline(&buffer, &bufsize, fpIn) == -1){exit(0);}
-            char * token;
+            if(getline(&buffer, &bufsize, fpIn) == -1){break;
+            }
             char* rest = buffer;
             char** arguments;
-            char spacer[2] = " ";
-            while (arguments = tokenize((token = strtok_r(rest, "&", &rest)), spacer)){
-                if(fork() == 0){
-                if(-1 == execvp(arguments[0],arguments)){printf("not run\n");}}
+            char * token;
+            while (token = strtok_r(rest, "&", &rest)){
+                arguments = tokenize(token, spacer);
+                if(!defCmd(arguments)){
+                    if(fork() == 0){
+                        if(-1 == execvp(arguments[0],arguments)){printf("not run\n");exit(0);}}
+                }
             }
+            wait(&status);
+            if(argc == 1){printf("wish>");}
         }
         
 }
