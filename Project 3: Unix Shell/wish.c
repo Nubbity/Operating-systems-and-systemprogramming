78f1aeea88eab7d88366bb6405c06b12114480d0
Santeri <santeri.hynninen@student.lut.fi>
2022-04-19 02:01:42 +0300

    All features implemented

--- Project 3: Unix Shell/wish.c
+++ Project 3: Unix Shell/wish.c
@@ -18,7 +18,6 @@
 
 #define PATH "/bin"
 
-#define ERROR_ACCESS "Could not access the command.\n"
 #define ERROR_EXIT "not able to use given arguments with exit\n"
 #define ERROR_CD "Give excactly one argument with cd\n"
 #define ERROR_OWD "failed to get owd\n"
@@ -26,7 +25,9 @@
 #define ERROR_MEMORY "Failed to allocate memory\n"
 #define ERROR_FILE "Failed to open the file\n"
 #define ERROR_EXECV "Failed to run the command\n"
+#define ERROR_ACCESS "Failed to access the command\n"
 
+//Splits invidual command to array
 char** tokenize(char* str, char *spacer)
 {
     str[strcspn(str, "\n")] = 0;//removes new line from the command
@@ -43,7 +44,7 @@ char** tokenize(char* str, char *spacer)
 int defCmd(char ** arguments){//return 1 if not found and 0 if one found
     char str[PATH_MAX] = "PATH=";
     char ss[2] = ":";
-    if(!strcmp(arguments[0], "exit")){
+    if(!strcmp(arguments[0], "exit")){//Exit -exits from shell
         if(!arguments[1]){
             printf("Exiting wish\n");
             exit(0);}
@@ -52,10 +53,10 @@ int defCmd(char ** arguments){//return 1 if not found and 0 if one found
         }
         return 1;
     }
-    else  if(!strcmp(arguments[0], "path")){
-        if(arguments[1] == NULL){setenv("PATH", "", 1);}
+    else  if(!strcmp(arguments[0], "path")){//Path -changes path used
+        if(arguments[1] == NULL){setenv("PATH", "", 1);}//clear path
         else{
-            for (int i = 1;; i++)
+            for (int i = 1;; i++)//set path to contain arguments seperated with :
             {   if(arguments[i] == NULL){break;}
                 putenv(strcat(str, arguments[i]));
                 strcat(str, ss);
@@ -63,7 +64,7 @@ int defCmd(char ** arguments){//return 1 if not found and 0 if one found
         }
         printf("New Path(s) are: %s\n", getenv("PATH"));
         return 1;}
-    else  if(!strcmp(arguments[0], "cd")){
+    else  if(!strcmp(arguments[0], "cd")){//cd- changes working directory
         if(!arguments[1] || arguments[2]){
             write(STDERR_FILENO, ERROR_CD, strlen(ERROR_CD));
         }
@@ -73,20 +74,22 @@ int defCmd(char ** arguments){//return 1 if not found and 0 if one found
             if (getcwd(owd, sizeof(owd))){
                 printf("Old working dir: %s\n", owd);}
             else{write(STDERR_FILENO, ERROR_OWD, strlen(ERROR_OWD));}
-            chdir(arguments[1]);
+            chdir(arguments[1]);//set new working directory
             if (getcwd(cwd, sizeof(cwd))) {
                 printf("New working dir: %s\n", cwd);}
             else{write(STDERR_FILENO, ERROR_CWD, strlen(ERROR_CWD));}
         }
         return 1;
     }
-    else  if(!strcmp(arguments[0], "help")){
+    else  if(!strcmp(arguments[0], "help")){//help -prints all build in commands
         printf("commands: exit, path, cd\n");
         return 1;
     }
     else{
         return 0;}
 }
+//Check where ourput is wanted -> used invidual per command
+//returns fd to the opened file and if not opened 0
 int checkOutput(char ** array){
     int fd = 0;
     for (int i = 0;; i++)
@@ -96,21 +99,47 @@ int checkOutput(char ** array){
             char path[PATH_MAX] = "./";
             strcat(path, array[i+1]);
             fd = open(path, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+            if(fd == 0){write(STDERR_FILENO, ERROR_FILE, strlen(ERROR_FILE));exit(1);}
             array[i] = 0;
             return fd;
         }
     }
 
 }
-
-
+//Check if command is accessable.
+//Returns 1 if command is found with /<path>/<command>
+//Tries all set paths
+int checkAccess(char* argument){
+   
+    char spacer[2] = ":";
+    char* rest =  getenv("PATH");
+    char* token;
+    char path[PATH_MAX];
+    
+    char cmd[300] = "/";
+    int i = 0;
+    strcat(cmd, argument);
+    
+    while ((token = strtok_r(rest, spacer, &rest))){
+        
+        strcpy(path, token);
+        strcat(path, cmd);
+        
+        if(0 == access(path, X_OK)){
+            printf("%s", path);
+            return 1;}
+        i++;
+    }
+    return 0;
+}
+//main function
 int main(int argc, char *argv[]){
     setenv("PATH", PATH, 1);//Set path to only contain /bin    
-    FILE * fpIn = stdin;
-    FILE * fpOut = stdout;
+    FILE * fpIn = stdin;//standard input
+    FILE * fpOut = stdout;//standard output
     int fdOut;
     char *buffer;
-    size_t bufsize = 0;
+    size_t bufsize = 300;
     int status;
     char spacer[2] = " ";
     
@@ -123,7 +152,7 @@ int main(int argc, char *argv[]){
     if(argc == 1){
         fpIn = stdin;
     }
-    else if(argc == 2){
+    else if(argc == 2){//custom input
         fpIn = fopen(argv[1], "r");
 
     
@@ -140,27 +169,26 @@ int main(int argc, char *argv[]){
     if(argc == 1){printf("wish>");}
     //wish loop starts
         while(1){
-            if(getline(&buffer, &bufsize, fpIn) == -1){break;
-            }
+            if(getline(&buffer, &bufsize, fpIn) == -1){exit(0);}//gets command line
             char* rest = buffer;
             char** arguments;
             char * token;
-            while (token = strtok_r(rest, "&", &rest)){
-                arguments = tokenize(token, spacer);
-                if(!defCmd(arguments)){
-                    if(fork() == 0){
-                        if ((fdOut = checkOutput(arguments))){
-                            dup2(fdOut, STDOUT_FILENO);
+            while (token = strtok_r(rest, "&", &rest)){//splits commands with &
+                arguments = tokenize(token, spacer);//command is put to array
+                if(!defCmd(arguments)){//checks if command is build in 
+                    if(fork() == 0){//creates child process to execute the command, mother process skips
+                        if ((fdOut = checkOutput(arguments))){//if custom ouput give <command> > <output>
+                            dup2(fdOut, STDOUT_FILENO);//errors and std ouput directed to custom output
                             dup2(fdOut, STDERR_FILENO);
                         }
-                        
-                        if(-1 == execvp(arguments[0],arguments)){
-                            write(STDERR_FILENO, ERROR_EXECV, strlen(ERROR_EXECV));}
-                        close(fdOut);
-                        exit(0);}
+                        if(checkAccess(arguments[0]) == 0){write(STDERR_FILENO, ERROR_ACCESS, strlen(ERROR_ACCESS));exit(1);}
+                        if(-1 == execvp(arguments[0],arguments)){//executes command
+                            write(STDERR_FILENO, ERROR_EXECV, strlen(ERROR_EXECV));exit(1);}
+                        }
                 }
             }
-            wait(&status);
+            
+            wait(&status);//mother process waits
             if(argc == 1){printf("wish>");}
         }
         
